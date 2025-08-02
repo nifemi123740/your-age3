72252f042aa9dc4599a2aa625302621fbc026ce7
nifemi123740 <deborahadegbite407@gmail.com>
2025-08-02 13:34:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+age=int(input("Enter your age: "))
+
+if age>20:
+    print("❌User is not eligible.")
+
+if age==20:
+    print("✅User is eligible.")
+
+if age==10:
+    print("✅User is eligible.")
+
+if age>10 and age<20:
+    print("✅User is eligible.")
+
+if age <10:
+    print("❌User is not eligible.")
